4080e45bad997d703bf552ff07e45dd562d908d3
Thanh Tien <vothanhtien190901@gmail.com>
2023-06-26 16:22:35 +0700

    edit the file package.json and .posthtmlrc.js

---
+++ html-css-training/practice-one/src/.posthtmlrc.js
@@ -0,0 +1,11 @@
+const posthtmlInclude = require('posthtml-include');
+
+const config = {
+  plugins: [
+    posthtmlInclude({
+      root: __dirname + '/',
+    }),
+  ],
+};
+
+module.exports = config;
